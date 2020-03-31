8acf285d9b4fb8fb0bbcc620bbb5d8913f279fff
Tuure Takala <tuure.takala@seamk.fi>
2020-03-31 12:45:57 +0300

    Working robot factory commit

--- app.js
+++ app.js
@@ -2,55 +2,45 @@ function sleep(ms){
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-// Alustetaan yhteys robotin http sivulle
-// const express = require('express');
 const axios = require('axios');
+const mqtt = require('mqtt');
 
-// let app = express();
+const main_loop = () => {
+    setTimeout(() => {
+        let start_time_stamp = new Date();
 
-axios.get('https://fanuc-robot-http-server.herokuapp.com/')
-    .then((res) => {
-        console.log(res);
+        axios.get('https://fanuc-robot-http-server.herokuapp.com/')
+            .then((res) => {
+                const time_stamp = new Date();
+                const delta = time_stamp - start_time_stamp;
+                start_time_stamp = time_stamp;
+                const regexp = 'Joint   [1-6]: *(-?.*)';
+                let joints = [];
+                let matches = res.data.matchAll(regexp);
+                let count = 0;
+                for (const match of matches) {
+                    count++;
+                    if (count > 6) break;
+                    const value = parseFloat(match[1]);
+                    joints.push(value);
+                }
 
+                let data = {
+                    time: time_stamp,
+                    joints: joints
+                };
 
-        const regexp = '-?\d*\.([0-9])\d+';
-        let joints = [];
-        let matches = res.data.matchAll(regexp);
-        let count = 0;
-        for (const match of matches) {
-            count++;
-            if (count > 6) break;
-            const value = parseFloat(match[1]);
-            joints.push(value);
-        }
+                mqtt_client.publish('joints', JSON.stringify(data));
+
+                console.log(start_time_stamp, joints, delta, 'ms');
+                main_loop();
+            });
+    }, 10);
+}
+
+const mqtt_client = mqtt.connect('wss://auoh-mqtt-tuure.herokuapp.com');  // wss://mqtt-broker-mikko.herokuapp.com
+mqtt_client.on('connect', () => {
+    console.log('connected to mqtt broker');
+    main_loop();
 });
-
-// -?\d*\.([0-9])\d+
-// Regexr antoi yläpuolella olevalla arvolla lukemat --> testataan
-// const regexp = '-?\d*\.([0-9])\d+';
-//     let joints = [];
-//     let matches = res.data.matchAll(regexp);
-//     let count = 0;
-//     for (const match of matches) {
-//         count++;
-//         if (count > 6) break;
-//         const value = parseFloat(match[1]);
-//         joints.push(value);
-//     }
-
-
-
-// // Main loop, jossa ohjelma pyörii ja kyselee akseleiden arvoja.
-// const main_loop = () => {
-//     setTimeout(() => {
-
-    
-//         const time_stamp = new Date();
-//         const delta = time_stamp - start_time_stamp;
-//         main_loop();
-//     }, 10);
-// }
-
-// main_loop();
-
-
+  
